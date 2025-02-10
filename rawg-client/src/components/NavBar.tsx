3589561a7b77931aca0dcff43762c528ef988505
AbdullahMarwan <abdu.moussa01@gmail.com>
2025-02-10 11:18:36 +0100

    Implemented darkmode switch

--- rawg-client/src/components/NavBar.tsx
+++ rawg-client/src/components/NavBar.tsx
@@ -1,11 +1,19 @@
-import { HStack, Image, Text} from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  HStack,
+  Image,
+  Switch,
+  Text,
+} from "@chakra-ui/react";
 import logo from "/src/assets/logo.png";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
     <HStack>
-      <Image src={logo} boxSize="60px"/>
-      <Text>NavBar</Text>
+      <Image src={logo} boxSize="60px" />
+      <ColorModeSwitch/>
     </HStack>
   );
 };
