e8fdaa6a95363a6fdda8860bdf96d8b912e66c34
AbdullahMarwan <abdu.moussa01@gmail.com>
2025-02-10 10:58:23 +0100

    Created a navbar

--- rawg-client/src/App.tsx
+++ rawg-client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     >
       <GridItem pl="2" bg="orange.300" area={"header"}>
         Header
+        <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
         <GridItem pl="2" bg="pink.300" area={"aside"}>
